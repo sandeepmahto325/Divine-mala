d45c501c3c4d93edc49fe34f6e926fc17b0ddf5d
sandeepmahto325 <sandeepmahto325@gmail.com>
2026-06-21 20:23:48 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'screens/main_shell.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.light,
+    systemNavigationBarColor: Colors.transparent,
+  ));
+
+  try {
+    await Firebase.initializeApp();
+  } catch (_) {}
+
+  runApp(
+    const ProviderScope(
+      child: DivineMalaApp(),
+    ),
+  );
+}
