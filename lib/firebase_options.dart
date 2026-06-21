c0460001d34b7b54b2a960e62132a1dd4f8c594d
sandeepmahto325 <sandeepmahto325@gmail.com>
2026-06-22 00:31:54 +0530

    Create firebase_options.dart

---
+++ lib/firebase_options.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      default:
+        throw UnsupportedError('Unsupported platform');
+    }
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'YOUR_ANDROID_API_KEY',
+    appId: '1:000000000000:android:0000000000000000',
+    messagingSenderId: '000000000000',
+    projectId: 'divine-mala-app',
+    storageBucket: 'divine-mala-app.appspot.com',
+  );
+}
